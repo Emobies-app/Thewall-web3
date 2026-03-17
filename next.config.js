4c08b0aac31ee71cb5aca28d1291d41faf70871d
Emobies & TheWall = Emowall 2.0 <meradivin@gmail.com>
2026-03-17 09:04:50 +0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,11 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      'pino-pretty': false,
       'porto/internal': false,
       'porto': false,
     }
     return config
   },
 }
-
-module.exports = nextConfig
