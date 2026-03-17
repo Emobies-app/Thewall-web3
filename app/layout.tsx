0d5afe3c68656cb66e524f82d144bc6939490136
Emobies & TheWall = Emowall 2.0 <meradivin@gmail.com>
2026-03-17 19:49:31 +0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
 import { EmowallAIChat } from '@/components/EmowallButterfly';
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html>
       <body>
         {children}
-        <EmowallAIChat />  {/* ← butterfly FAB globally */}
+        <EmowallAIChat />
       </body>
     </html>
   );
