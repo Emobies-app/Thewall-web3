8db066be401e978040c7c2f3755d623b711448ed
Emobies & TheWall = Emowall 2.0 <meradivin@gmail.com>
2026-03-17 11:06:33 +0400

    Create thewall_ultimate_logo.dart

---
+++ lib/widgets/thewall_ultimate_logo.dart
@@ -0,0 +1,16 @@
+import '../widgets/thewall_ultimate_logo.dart';
+
+// Splash screen:
+RepaintBoundary(
+  child: TheWallUltimateLogo(size: 300),
+)
+
+// App bar:
+RepaintBoundary(
+  child: TheWallUltimateLogo(size: 48),
+)
+
+// Login:
+RepaintBoundary(
+  child: TheWallUltimateLogo(size: 200),
+)
