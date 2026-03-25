23e781ff776ddda47f9f97ed7618bd5b08838e18
Emobies & TheWall = Emowall 2.0 <meradivin@gmail.com>
2026-03-25 07:30:28 +0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -8,6 +8,28 @@ const nextConfig = {
     }
     return config
   },
+
+  async headers() {
+    return [
+      {
+        source: '/sw.js',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-cache, no-store, must-revalidate',
+          },
+          {
+            key: 'Content-Type',
+            value: 'application/javascript; charset=utf-8',
+          },
+          {
+            key: 'Service-Worker-Allowed',
+            value: '/',
+          },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
